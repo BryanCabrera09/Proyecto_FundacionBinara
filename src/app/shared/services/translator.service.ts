f9a932a48ac9d7f84d4d6e4b663445f69964f23c
BryanCabrera09 <81339678+BryanCabrera09@users.noreply.github.com>
2023-10-02 17:54:50 -0500

    Update

--- src/app/shared/services/translator.service.ts
+++ src/app/shared/services/translator.service.ts
@@ -10,8 +10,8 @@ export class TranslatorService {
   public lang: any = 'es';
 
   private headers = new HttpHeaders().set('Content-Type', 'application/json')
-    .set('Ocp-Apim-Subscription-Key', '')   /* Nueva Key: 1fb564ddad3245dbae5a9ff4a840fc41 */
-    .set('Ocp-Apim-Subscription-Region', '');   /* Region: eastus2 */
+    .set('Ocp-Apim-Subscription-Key', '')   /* Nueva Key: 1fb564ddad3245dbae5a9ff4a840fc41   o  ca19a4d477f14449a78b013d3a63aa27 */
+    .set('Ocp-Apim-Subscription-Region', '');   /* Region: eastus2    o    eastus */
 
   private URL = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=';
 
